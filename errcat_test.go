edc9667b2b5b57381e86b6880b415d6d14e73718
Eric Myhre <hash@exultant.us>
2017-08-31 20:26:03 +0200

    test deserialization.
    Here we run into "Fun" in the Dwarf Fortress sense of the word:
    
    Go's type system just *isn't very good*.
    
    Ideally we want to declare a bunch of `const( ErrAsdf ThePkgErrorCategory = "whatever"; [...])` stuff, and have switch statements on `switch errcat.Category(err) {` with `case ErrAsdf:` in that switch, right?
    
    So that's easy enough and WeGotThis in plain ol go code.  *Slight* hankyness, because your constructor funcs and `Category()` take and return `interface{}` wildcards -- there's no way to declare `func Errorf(category Generic[kind=string]) error`, so if we want to accept people's typedef'd types, we're stuck going full on `interface{}`.  Similarly, we return an `interface{}` from `Category(error) interface{}` and that lets us switch on the result directly (it doesn't check you at compile time if you compare to some other type that "shouldn't" be here, but that's simply not possible without declaring a full type and set of helper funcs per set of category enums, which... we're not doing).
    
    Now when we get to round-tripping serialization formats, are we any better off?
    
    No.
    
    Deserializing into our concrete error type will result in a `string`-kind with no additional type info -- because how would it be otherwise?
    
    And these type-less strings are **not equal** to your typed string constants you may have declared in your API packages.
    
    (No, setting `e2.Category_ = ErrorCategory("")` as a hint before unmarshal does not help if the `Category_` field is still `interface{}`: string kinds are not themselves settable, so stdlib json.Unmarshal will still replace that value with a less-typeinfo'd one.)
    
    So: if you want to deserialize these errors, and have a typed category on them: declare your own concrete type for receiving the deserialization.  Copypaste the necessary interface methods.  It'll work.  The good news is: it's only 10 lines of ugly.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- errcat_test.go
+++ errcat_test.go
@@ -32,12 +32,32 @@ func TestErrorf(t *testing.T) {
 }
 
 func TestSerialization(t *testing.T) {
-	err := errcat.Errorf(ErrAsdf, "asdf: %s", "fmtme")
-	bytes, err := json.Marshal(err)
+	e1 := errcat.Errorf(ErrAsdf, "asdf: %s", "fmtme")
+	bytes, err := json.Marshal(e1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if string(bytes) != `{"category":"err-asdf","message":"asdf: fmtme"}` {
-		t.Errorf("must match fixture -- got `%s`", string(bytes))
-	}
+	t.Run("must match fixture", func(t *testing.T) {
+		if string(bytes) != `{"category":"err-asdf","message":"asdf: fmtme"}` {
+			t.Errorf("must match fixture -- got `%s`", string(bytes))
+		}
+	})
+	t.Run("must roundtrip", func(t *testing.T) {
+		// Deserializing is interesting because if you want the category to be comparable with typeinfo,
+		// you have to declare your own struct with that info.
+		// Or, use a filter func to coerce it.
+		type deserErr struct {
+			Category_ ErrorCategory     `json:"category"          refmt:"category"`
+			Message_  string            `json:"message"           refmt:"message"`
+			Details_  map[string]string `json:"details,omitempty" refmt:"category,omitempty"`
+		}
+		var e2 deserErr
+		err := json.Unmarshal(bytes, &e2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if e2.Category_ != errcat.Category(e1) {
+			t.Errorf("category must match after roundtrip json -- got `%s`", e2.Category_)
+		}
+	})
 }
