57f4c4b14ae1d4644a3009f5758293dfe781fb2d
Eric Myhre <hash@exultant.us>
2017-09-19 12:51:19 +0200

    add test helper function for checking categories.
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ errcatTestutil.go
@@ -0,0 +1,42 @@
+package errcat
+
+import "fmt"
+
+/*
+	ShouldErrorWithCategory is a helper function for GoConvey-style testing:
+	it takes an "actual" value (which should be an error), and a description
+	of what errcat "Category" it should have, and returns
+	either an empty string if the error matches the category,
+	or a string describing how it's out of line if there's no match.
+	(In other words: return of empty string means "success!",
+	and any other string means "failure: because %s".)
+
+	(It's easy to include it in this package because the GoConvey
+	assertion functions don't require any GoConvey *types*, so we don't
+	have to add any dependencies in order to provide these helpers.
+	Ignore this function if you don't use GoConvey.)
+*/
+func ShouldErrorWithCategory(actual interface{}, expectedClause ...interface{}) string {
+	if len(expectedClause) != 1 {
+		return "Misuse: ShouldErrorWithCategory predicate needs exactly one item in the \"expected\" clause"
+	}
+	expected := expectedClause[0]
+	if actual == nil && expected == nil {
+		return "" // good!
+	}
+	if actual == nil {
+		return fmt.Sprintf("Actual: nil\nExpected category: %q", expected)
+	}
+	_, ok := actual.(error)
+	if !ok {
+		return fmt.Sprintf("Actual: %v\nExpected category: %q\nShould have error interface type!  (This is probaby a misuse of the ShouldErrorWithCategory predicate...)", actual, expected)
+	}
+	e2, ok := actual.(Error)
+	if !ok {
+		return fmt.Sprintf("Actual: %v\nExpected category: %q\nShould have an errcat error!  Was type %T.", actual, expected, actual)
+	}
+	if e2.Category() != expected {
+		return fmt.Sprintf("Actual category: %q\nExpected category: %q\n(Full error: %v)", e2.Category, expected, actual)
+	}
+	return "" // couldn't find grounds to reject it; must be good!
+}
