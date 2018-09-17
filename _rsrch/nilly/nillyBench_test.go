a7aad03e83a20628d8bae00a913a94712275079f
Eric Myhre <hash@exultant.us>
2018-09-17 10:35:21 +0200

    Apparently, not much.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- _rsrch/nilly/nillyBench_test.go
+++ _rsrch/nilly/nillyBench_test.go
@@ -19,10 +19,19 @@ import (
 
 // Ballpark results:
 //
-//		BenchmarkNilCheck-8             2000000000               0.27 ns/op            0 B/op          0 allocs/op
-//		BenchmarkReflectNilCheck-8      300000000                3.98 ns/op            0 B/op          0 allocs/op
+//		BenchmarkNilCheck-8                     2000000000               0.27 ns/op            0 B/op          0 allocs/op
+//		BenchmarkReflectNilCheck-8              300000000                3.98 ns/op            0 B/op          0 allocs/op
+//		BenchmarkReflectNilCheckCorrectly-8     500000000                3.50 ns/op            0 B/op          0 allocs/op
 //
 // Twelve times slower.  Unfortunately significant.
+//
+// And further, this actually gets worse if you use a struct rather than pointer,
+// because now you'll get allocs in there somewhere:
+//
+//		BenchmarkNilCheck_Nonptr-8                      2000000000               0.27 ns/op            0 B/op          0 allocs/op
+//		BenchmarkReflectNilCheckCorrectly_Nonptr-8      30000000                49.1 ns/op            48 B/op          1 allocs/op
+//
+// Conclusion: this is basically a no-go if we want this library to be near-zero cost.
 //
 
 // Related learning: changing all of the interface methods to only work on the
@@ -59,6 +68,17 @@ func BenchmarkReflectNilCheck(b *testing.B) {
 	}
 }
 
+func BenchmarkReflectNilCheckCorrectly(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ex := exampleContainer{}
+		err := errcat.Error(ex.Error)
+		rv := reflect.ValueOf(err)
+		if rv.Kind() == reflect.Ptr && rv.IsNil() == false {
+			panic("reflect should have caught this")
+		}
+	}
+}
+
 type exampleContainer struct {
 	Error *errStruct
 }
@@ -73,6 +93,36 @@ func (e errStruct) Category() interface{}      { return e.Category_ }
 func (e errStruct) Message() string            { return e.Message_ }
 func (e errStruct) Details() map[string]string { return e.Details_ }
 func (e errStruct) Error() string              { return e.Message_ }
+
+func BenchmarkNilCheck_Nonptr(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ex := exampleContainer2{}
+		err := errcat.Error(ex.Error)
+		if err == nil {
+			panic("typed nils don't work this way")
+		}
+	}
+}
+
+func BenchmarkReflectNilCheck_Nonptr(b *testing.B) {
+	b.Skip("this would panic because reflect.Value.IsNil() is rude")
+}
+
+func BenchmarkReflectNilCheckCorrectly_Nonptr(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ex := exampleContainer2{}
+		err := errcat.Error(ex.Error)
+		rv := reflect.ValueOf(err)
+		if rv.Kind() == reflect.Ptr && rv.IsNil() == false {
+			panic("reflect should have caught this")
+		}
+	}
+}
+
+// all the same except *not* a pointer to errStruct
+type exampleContainer2 struct {
+	Error errStruct
+}
 
 //func BenchmarkSanityCheck(b *testing.B) {
 //	for n := 0; n < b.N; n++ {
